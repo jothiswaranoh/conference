ad5b2643ea7a344149b5735c41e91f3945063ff3
JayaSamuthraDevi <115087700+JayaSamuthraDevi@users.noreply.github.com>
2024-02-13 15:54:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,10 @@ ini_set('display_errors', 1); //showing errors in browser
 include 'libs/load.php';
 include '_partials/_head.php';
 include '_partials/_header.php';
+include '_partials/_conference_topics.php';
+include '_partials/_organising_committee.php';
 include '_partials/_publication_info.php';
 include '_partials/_imp_date.php';
+include '_partials/_guidelines.php';
 include '_partials/_contact.php';
 include '_partials/_footer.php';
