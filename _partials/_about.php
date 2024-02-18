851b95d4e89de20207086fe0c45c5d3ad8d7b373
JayaSamuthraDevi <jeevitham811@gmail.com>
2024-02-18 11:54:02 +0530

    commit

--- _partials/_about.php
+++ _partials/_about.php
@@ -3,19 +3,19 @@
   <div class="container position-relative" data-aos="fade-up">
     <div class="row">
       <div class="col-lg-6">
-        <h2>About The Event</h2>
+        <h2 class="text-p">About The Event</h2>
         <p>Embark on a journey of discovery and innovation with ICIRET 2024, where brilliance converges
          to shape the future of engineering and technology. As the curtain rises on this prestigious event, 
          we invite you to delve into a realm of cutting-edge discussions, insightful presentations,
           and collaborative opportunities.</p>
       </div>
       <div class="col-lg-3">
-        <h3>Where</h3>
+        <h3 class="text-p">Where</h3>
         <p>Park College of Engineering and Technology, Coimbatore</p>
       </div>
       <div class="col-lg-3">
-        <h3>When</h3>
-        <p>Sunnday to Monday<br>25-26 April</p>
+        <h3 class="text-p">When</h3>
+        <p>Sunday to Monday<br>25-26 April</p>
       </div>
     </div>
   </div>
