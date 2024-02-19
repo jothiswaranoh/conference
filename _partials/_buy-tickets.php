102dbc82fe6794635fd45c24a7d6318267da93a4
jothiswaran <72430882+jothiswaranoh@users.noreply.github.com>
2024-02-19 10:32:25 +0530

    Update _buy-tickets.php

--- _partials/_buy-tickets.php
+++ _partials/_buy-tickets.php
@@ -1,5 +1,5 @@
 <!-- ======= Registration Fee Section ======= -->
-<section id="registration-fee container-p" class="bg-primary-js">
+<section id="registration-fee" class="bg-primary-js">
   <div class="container" data-aos="fade-up">
 
     <div class="section-header text-white">
@@ -9,74 +9,53 @@
 
     <div class="row">
       <div class="col-lg-3" data-aos="fade-up" data-aos-delay="100">
-        <div class="solution_cards_box">
-          <div class="solution_card">
-            <div class="hover_color_bubble"></div>
-            <div class="solu_title">
-              <h3>Indian delegate / Author</h3>
-            </div>
-            <div class="solu_description">
-              <h6 class="card-price text-center">Rs. 2000</h6>
-              <hr>
-              <ul class="fa-ul ul-m text-center">
-                <li>For init. Conference</li>
-              </ul>
-            </div>
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body register-card">
+            <h5 class="card-title text-muted text-uppercase text-center text-c">Indian delegate / Author</h5>
+            <h6 class="card-price text-center">Rs. 2000</h6>
+            <hr>
+            <ul class="fa-ul ul-m text-center">
+              <li>For init. Conference</li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
-        <div class="solution_cards_box">
-          <div class="solution_card">
-            <div class="hover_color_bubble"></div>
-            <div class="solu_title">
-              <h3>Students & Research Scholars</h3>
-            </div>
-            <div class="solu_description">
-              <h6 class="card-price text-center">Rs. 2000</h6>
-              <hr>
-              <ul class="fa-ul ul-m text-center">
-                <li>For init. Conference</li>
-              </ul>
-            </div>
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body register-card">
+            <h5 class="card-title text-muted text-uppercase text-center text-c">Students & Research Scholars</h5>
+            <h6 class="card-price text-center">Rs. 2000</h6>
+            <hr>
+            <ul class="fa-ul ul-m text-center">
+              <li>For init. Conference</li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
-        <div class="solution_cards_box">
-          <div class="solution_card">
-            <div class="hover_color_bubble"></div>
-            <div class="solu_title">
-              <h3>Students & Research Scholars</h3>
-            </div>
-            <div class="solu_description">
-              <h6 class="card-price text-center">Rs. 1000</h6>
-              <hr>
-              <ul class="fa-ul ul-m text-center">
-                <li>For Innovation Expo</li>
-              </ul>
-            </div>
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body register-card">
+            <h5 class="card-title text-muted text-uppercase text-center text-c">Students & Research Scholars</h5>
+            <h6 class="card-price text-center">Rs. 1000</h6>
+            <hr>
+            <ul class="fa-ul ul-m text-center">
+              <li>For Innovation Expo</li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="col-lg-3" data-aos="fade-up" data-aos-delay="300">
-        <div class="solution_cards_box">
-          <div class="solution_card">
-            <div class="hover_color_bubble"></div>
-            <div class="solu_title">
-              <h3>Foreign  Papers /   delegates </h3>
-            </div>
-            <div class="solu_description">
-              <h6 class="card-price text-center"> US $ 100</h6>
-              <hr>
-              <p class="text-center">
-                For init. Conference
-              </p>
-            </div>
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body register-card">
+            <h5 class="card-title text-muted text-uppercase text-center text-c">Foreign Delegates</h5>
+            <h6 class="card-price text-center">US $ 100</h6>
+            <hr>
+            <ul class="fa-ul ul-m text-center">
+              <li>For init. Conference</li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </section><!-- End Registration Fee Section -->
