45918c2500d12e9828b0be3f0497e92d769435a3
stark <stark@starks-MacBook-Air.local>
2024-03-03 12:11:44 +0530

    update the images

--- _partials/_speacker.php
+++ _partials/_speacker.php
@@ -33,4 +33,35 @@
               </div>
             </div>
           </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6">
+            <div class="speaker" data-aos="fade-up" data-aos-delay="100">
+              <img src="assets/img/speakers/principal.jpg" alt="Speaker 1" class="img-fluid">
+              <div class="details">
+                <h3><a href="speaker-details.html">Conference Chairman</a></h3>
+                <p>Dr. D. Lakshmanan [Principal]<br>Park College of Engineering and Technology</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="speaker" data-aos="fade-up" data-aos-delay="200">
+              <img src="assets/img/speakers/5.jpeg" alt="Speaker 2" class="img-fluid">
+              <div class="details">
+                <h3><a href="speaker-details.html">Patron</a></h3>
+                <p>Mrs. Anusha.R [Chief Executive]<br>CEO of Park Group of Institutions</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="speaker" data-aos="fade-up" data-aos-delay="300">
+              <img src="assets/img/speakers/6.jpg" alt="Speaker 3" class="img-fluid">
+              <div class="details">
+                <h3><a href="speaker-details.html">Conference Chairman</a></h3>
+                <p>Dr. D. Lakshmanan [Principal]<br>Park College of Engineering and Technology</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section><!-- End Speakers Section -->
